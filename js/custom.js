dcf67a1b9cd9f951b5f7c8cc1e864a7cd70e9e70
Jitesh Singh <jiteshsingh1709@gmail.com>
2018-11-23 12:57:38 +0530

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+$(document).ready(function() {
+	$('.leader-signup').click(function(e) {
+		$('#leader-sign-up').show(500);
+		$('#voter-registration').hide(500);
+		$('#normal-login').hide(500);
+		e.preventDefault();
+	});
+	$('.voter').click(function(e) {
+		$('#voter-registration').show(500);
+		$('#leader-sign-up').hide(500);
+		$('#normal-login').hide(500);
+		e.preventDefault();
+	});
+	$('.login').click(function(e) {
+		$('#normal-login').css('display','block',);
+		$('#normal-login').show(500);
+		$('#voter-registration').hide(500);
+		$('#leader-sign-up').hide(500);
+		e.preventDefault();
+	});
+});
